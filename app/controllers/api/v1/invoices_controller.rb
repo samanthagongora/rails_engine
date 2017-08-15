4c56ccc0fc3585af69f0926e483c7328fa9f6170
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-15 07:56:59 -0600

    takes out create, update, and destroy function for invoices because we dont need that :P

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -6,22 +6,4 @@ class Api::V1::InvoicesController < ApplicationController
   def show
     render json: Invoice.find(params[:id])
   end
-
-  def create
-    render json: Invoice.create(invoice_params)
-  end
-
-  def update
-    render json: Invoice.update(params[:id], invoice_params)
-  end
-
-  def destroy
-    Invoice.delete(params[:id])
-  end
-
-  private
-
-  def invoice_params
-    params.require(:invoice).permit(:status, :customer_id, :merchant_id, :created_at, :updated_at)
-  end
 end
