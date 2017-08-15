0009902e6e39a2dbc65e725b82e99ec00ce1e8bc
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-15 13:57:14 -0600

    adds find one item test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -23,4 +23,50 @@ describe "Items API" do
     expect(response).to be_success
     expect(item['id']).to eq(id)
   end
+
+  it "can get one item by attribute" do
+    dummy = create(:item)
+
+    get "/api/v1/items/find?name=#{dummy.name}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item['id']).to eq(id)
+  end
+
+  xit "can get one item by another attribute" do
+    merchant = create(:merchant)
+    id = create(:item, merchant: merchant).id
+
+    get "/api/v1/items/find?merchant_id=#{merchant.id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item['id']).to eq(id)
+  end
+
+  xit "can get all items by attribute" do
+    customer = create(:customer)
+    create_list(:item, 4, customer: customer)
+
+    get "/api/v1/items/find_all?customer_id=#{customer.id}"
+
+    items = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(items.count).to eq(4)
+  end
+
+  xit "can get all items by another attribute" do
+    create_list(:item, 4, status: 'shipped')
+
+    get "/api/v1/items/find_all?status=shipped"
+
+    items = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(items.count).to eq(4)
+  end
 end
