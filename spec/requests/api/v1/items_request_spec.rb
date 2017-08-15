8c236f2aabd57488a6ce328b32a41792ad7fa196
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-15 13:16:25 -0600

    adds functionality for show and index for items

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'date'
 
 describe "Items API" do
-  it "sends a list of invoices" do
+  it "sends a list of items" do
     create_list(:item, 3)
 
     get '/api/v1/items'
@@ -14,7 +14,7 @@ describe "Items API" do
     expect(items.count).to eq 3
   end
 
-  xit "can get one item by its id" do
+  it "can get one item by its id" do
     id = create(:item).id
 
     get "/api/v1/items/#{id}"
@@ -23,16 +23,5 @@ describe "Items API" do
 
     expect(response).to be_success
     expect(item['id']).to eq(id)
-  end
-
-  xit "can get one invoice by one attribute" do
-    invoice = create(:invoice)
-
-    get "/api/v1/invoices/find?status=shipped"
-
-    invoice = JSON.parse(response.body)
-
-    expect(response).to be_success
-    expect(invoice['id']).to eq(id)
   end
 end
