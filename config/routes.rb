2372c25a2e6bd28e562fd21bf88c1cf1b045a7ae
wratterman <will.ratterman@gmail.com>
2017-08-15 14:47:31 -0600

    find, find_all, and random now working and tested for transactions and customers

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,17 @@ Rails.application.routes.draw do
          get '/find_all', to: 'find#index'
          get '/random', to: 'random#show'
        end
+       namespace :customers do
+         get '/find', to: 'find#show'
+         get '/find_all', to: 'find#index'
+         get '/random', to: 'random#show'
+       end
+       namespace :transactions do
+         get '/find', to: 'find#show'
+         get '/find_all', to: 'find#index'
+         get '/random', to: 'random#show'
+       end
+
        resources :merchants, only: [:index, :show]
        resources :transactions, only: [:index, :show]
        resources :customers, only: [:index, :show]
--- spec/requests/api/v1/customer_requests_spec.rb
+++ spec/requests/api/v1/customer_requests_spec.rb
@@ -23,4 +23,40 @@ describe "Customers API" do
     expect(customer["id"]).to eq(cust.id)
     expect(customer["first_name"]).to eq(cust.first_name)
   end
+
+  it "can find a customer by it's name" do
+    create_list(:customer, 3)
+    show_cust = Customer.first
+    get "/api/v1/customers/find?name=#{show_cust.first_name}"
+
+    customer = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_success
+    expect(customer[:id]).to eq(show_cust.id)
+    expect(customer[:first_name]).to eq(show_cust.first_name)
+    expect(customer[:last_name]).to eq(show_cust.last_name)
+  end
+
+  it "can find all customer by shared name" do
+    create_list(:customer, 3)
+    show_cust = Customer.first
+    get "/api/v1/customers/find_all?name=#{show_cust.first_name}"
+
+    customers = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_success
+    expect(customers.count).to eq(3)
+  end
+
+  it "can return a random customer" do
+    c1 = create(:customer, first_name: "Joey")
+    c2 = create(:customer, first_name: "Amy")
+    c3 = create(:customer, first_name: "Tyler")
+
+    get "/api/v1/customers/random"
+
+    customer = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_success
+    expect(customer.count).to eq(1)
+    expect([c1.first_name, c2.first_name, c3.first_name]).to include(customer.first[:first_name])
+  end
 end
