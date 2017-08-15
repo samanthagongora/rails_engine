81fc86c9cd54fd26a4dcdee94842c9bfa1705295
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-15 14:17:50 -0600

    adds functionality for find one invoice item

---
+++ app/controllers/api/v1/invoice_items/find_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::InvoiceItems::FindController < ApplicationController
+  def show
+    render json: InvoiceItem.find_by(invoice_items_params)
+  end
+
+  def index
+    render json: InvoiceItem.where(invoice_items_params)
+  end
+
+  private
+
+  def invoice_items_params
+    params.permit(:id, :invoice_id, :item_id, :quantity, :unit_price, :created_at, :updated_at)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
         get 'find_all', to: 'find#index'
       end
 
+      namespace :invoice_items do
+        get 'find', to: 'find#show'
+        get 'find_all', to: 'find#index'
+      end
+
       resources :items, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
       resources :invoices, only: [:index, :show]
