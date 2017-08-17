92eb753b93252fd6efd29ed584c83df304875586
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-17 13:24:14 -0600

    adds functionality for customer invoices

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,7 @@ Rails.application.routes.draw do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
         get '/random', to: 'random#show'
+        get '/:id/invoices', to: 'invoices#index'
       end
 
       namespace :transactions do
