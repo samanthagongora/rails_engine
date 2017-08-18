47d4bdde1993962204c6d1972c46dd7e24a5889d
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-17 18:47:50 -0600

    adds serializers for formatting and sanitizing data

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -4,12 +4,20 @@ class Api::V1::Items::FindController < ApplicationController
   end
 
   def index
+    # binding.pry
     render json: Item.where(item_params)
   end
 
   private
 
   def item_params
+    if params[:unit_price]
+      params[:unit_price] = (params[:unit_price].to_f * 100).round(2).to_s
+    elsif params[:updated_at]
+      params[:updated_at] = params[:updated_at].to_datetime
+    elsif params[:created_at]
+      params[:created_at] = params[:created_at].to_datetime
+    end
     params.permit(:id, :name, :merchant_id, :description, :unit_price, :created_at, :updated_at )
   end
 end
--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -5,6 +5,6 @@ class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
     merchant = Merchant.find(params[:merchant_id])
-    render json: merchant.revenue_by_date(params[:date])
+    render json: merchant.revenue_by_date(params[:date]), serializer: RevenueSerializer
   end
 end
