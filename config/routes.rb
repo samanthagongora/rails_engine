4bd361460be7e066f870b0bb2668f701fc89bcff
wratterman <will.ratterman@gmail.com>
2017-08-17 22:37:36 -0600

    items/most_items?quantity= tested and passing

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
         get 'find_all', to: 'find#index'
         get 'random', to: 'random#show'
       end
-      
+
       namespace :merchants do         get '/find', to: 'find#show'
          get '/find_all', to: 'find#index'
          get '/random', to: 'random#show'
